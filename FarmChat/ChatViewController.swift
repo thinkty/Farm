b324bb7d773659237549458f5eac9aea8242bfd4
Kim TY <kimty@pal-nat186-39-106.itap.purdue.edu>
2018-12-02 15:12:09 -0500

    fixed the display messages function made the ui look better than before

--- FarmChat/ChatViewController.swift
+++ FarmChat/ChatViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 import Firebase
 
-class ChatViewController: UIViewController, UITextFieldDelegate  {
+class ChatViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
 
     
+    @IBOutlet var userMessageView: UITextView!
+    @IBOutlet var userName: UITextField!
+    @IBOutlet var OpponentName: UITextField!
+    @IBOutlet var OpponentMessageView: UITextView!
     @IBOutlet var messageView: ChatRoomCell!
     @IBOutlet var heightConstraint: NSLayoutConstraint!
     @IBOutlet var messageTextField: UITextField!
@@ -21,60 +25,120 @@ class ChatViewController: UIViewController, UITextFieldDelegate  {
         super.viewDidLoad()
         
         messageTextField.delegate = self
+        userName.delegate = self
+        userMessageView.delegate = self
+        OpponentName.delegate = self
+        OpponentMessageView.delegate = self
         
+        OpponentMessageView.layer.cornerRadius = 15.0
+        OpponentMessageView.layer.borderWidth = 2.0
+        userMessageView.layer.cornerRadius = 15.0
+        userMessageView.layer.borderWidth = 2.0
         
         // Do any additional setup after loading the view.
+        
+        retrieveMessages()
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        if textField == userName || textField == OpponentName {
+            return false
+        }
+        return true
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        UIView.animate(withDuration: 0.5) {
-            self.heightConstraint.constant = 308
+        UIView.animate(withDuration: 0.2) {
+            self.heightConstraint.constant = 360
             self.view.layoutIfNeeded()
         }
     }
     
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        UIView.animate(withDuration: 0.5) {
+        UIView.animate(withDuration: 0.2) {
             self.heightConstraint.constant = 50
             self.view.layoutIfNeeded()
         }
     }
+    
+    
     
     @IBAction func sendPressed(_ sender: Any) {
         messageTextField.endEditing(true)
         messageTextField.isEnabled = false
         sendButton.isEnabled = false
         
-        let messageDB = Database.database().reference().child("Messages")
-        let messageDictionary = ["Sender": Auth.auth().currentUser?.uid,
-                                 "MessageBody": messageTextField.text]
-        
-        messageDB.childByAutoId().setValue(messageDictionary) {
-            (error, reference) in
-            
-            if error != nil {
-                print(error!)
-            }
-            else {
-                print("Message saved on the database successfully!")
-            }
-            
+        if messageTextField.text == "" {
             self.messageTextField.isEnabled = true
             self.sendButton.isEnabled = true
             self.messageTextField.text = ""
+        } else {
+        
+            let messageDB = Database.database().reference().child("Messages")
+        
+            let messageDictionary = ["Sender": (Auth.auth().currentUser?.uid)!, "MessageBody": messageTextField.text!]
+        
+            messageDB.child((Auth.auth().currentUser?.uid)!).updateChildValues(messageDictionary) {
+                (error, reference) in
+            
+                if error != nil {
+                    print(error!)
+                }
+                else {
+                    print("Message saved on the database successfully!")
+                }
+            
+                self.messageTextField.isEnabled = true
+                self.sendButton.isEnabled = true
+                self.messageTextField.text = ""
+            }
         }
+        
+        retrieveMessages()
+    } //send pressed method
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        sendPressed(self)
+        return true;
     }
     
+    //retrieve message
+    func retrieveMessages() {
+        let MessageDB = Database.database().reference().child("Messages")
+        let usernameDB = Database.database().reference().child("Usernames")
+        var text: String = ""
+        var sender: String = ""
+        var senderUsername: String = ""
+        
+        MessageDB.observe(.childAdded, with: { (snapshot) in
+            let snapshotValue = snapshot.value as! Dictionary<String, String>
+            
+            text = snapshotValue["MessageBody"]! //give the text
+            sender = snapshotValue["Sender"]! //give the sender
+            
+            print("\nCurrent User  : ", Auth.auth().currentUser!.uid)
+            print("Sender        : ", sender)
+            print("Text          : ", text)
+            
+            usernameDB.observe(.childAdded, with: { (snapshot) in
+                let snapshotValue = snapshot.value as! Dictionary<String, String>
+                
+                senderUsername = snapshotValue["username"]! //give the username
+                print("Username      : ", senderUsername)
+                
+                if (sender == Auth.auth().currentUser!.uid) {
+                    print("you made here")
+                    self.userName.text = senderUsername
+                    self.userMessageView.text = text
+                }
+                
+                //TODO: do i need an else case in case the sender is not the current user?
+                //      How will I handle that case?
+            })
+        })
+    } //retrieve messages method
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- FarmChat/ViewController.swift
+++ FarmChat/ViewController.swift
@@ -7,11 +7,10 @@
 //
 
 import UIKit
-import FirebaseAuth
-import FirebaseDatabase
+import Firebase
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
     var colorArray = [["red", "orange", "yellow", "green", "blue", "purple", "brown", "magenta", "tan", "cyan", "olive", "maroon", "navy", "silver", "violet", "pink", "black", "white", "gray"], ["beautiful", "clean", "adorable", "fancy", "funky", "old-fashioned", "plain"], ["clever", "dead", "famous", "intelligent", "important", "strong", "powerful", "shy", "rich", "wrong"], ["brave", "calm", "delightful", "happy", "jolly", "kind", "nice", "proud", "silly"], ["angry", "clumsy", "naughty", "fierce", "grumpy", "lazy", "mysterious", "nervous", "scary", "weird"], ["chubby", "crooked", "flat", "high", "skinny", "big", "tiny"], ["colossal", "fat", "gigantic", "great", "huge", "large", "little", "massive", "puny", "short", "small", "tall", "tiny", "hot"], ["loud", "noisy", "whispering", "thundering"]]
     
     var animalArray = ["cat", "bird", "dog", "sheep", "cattle", "goat", "horse", "chicken", "pig", "rabbit", "donkey", "duck", "llama", "mule", "calf", "pony", "emu", "goose", "cricket", "deer", "worm"]
@@ -27,30 +26,70 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        inputCustomName.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let border = CALayer()
+        let width = CGFloat(2.0)
+        border.borderColor = UIColor.lightGray.cgColor
+        border.frame = CGRect(x: 0, y: inputCustomName.frame.size.height - width, width: inputCustomName.frame.size.width, height: inputCustomName.frame.size.height)
+        border.borderWidth = width
+        inputCustomName.layer.addSublayer(border)
+        inputCustomName.layer.masksToBounds = true
+        
+        startButton.layer.cornerRadius = 15.0
+        startButton.layer.borderWidth = 2.0
+        
     }
     
-    @IBOutlet weak var inputCustomName: UITextField!
+    @IBOutlet var startButton: UIButton!
+    
+    @IBOutlet var inputCustomName: UITextField!
     
     @IBAction func startAnonymouslyDidTapped(_ sender: Any) {
+        
         if inputCustomName.hasText {
-            userName = inputCustomName.text ?? getAccountName()
-            //print("\(userName)")
+            userName = inputCustomName.text!
         } else {
             userName = getAccountName()
         }
-        print("\(userName)")
+        print("Anonymous user created with the username: \(userName).")
         
         Auth.auth().signInAnonymously() { (authResult, error) in
             if let error = error {
                 print("Sign in failed: ", error.localizedDescription)
             } else {
                 print("Signed in anonymously")
-                self.performSegue(withIdentifier: "toMain", sender: self)
                 
+                //TODO: Let the username be saved so that it can be accessed anytime
+                let usernameDB = Database.database().reference().child("Usernames")
+                let usernameDictionary = ["username": self.userName]
+                usernameDB.child((Auth.auth().currentUser?.uid)!).setValue(usernameDictionary)
+                
+                let messageDB = Database.database().reference().child("Messages")
+                
+                let messageDictionary = ["Sender": (Auth.auth().currentUser?.uid)!, "MessageBody": "\(self.userName) has entered the chatroom"]
+                
+                messageDB.child((Auth.auth().currentUser?.uid)!).setValue(messageDictionary) {
+                    (error, reference) in
+                    
+                    if error != nil {
+                        print(error!)
+                    }
+                    else {
+                        print("Message saved on the database successfully!")
+                        self.performSegue(withIdentifier: "toMain", sender: self)
+                    }
+                }
             }
         }
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        startAnonymouslyDidTapped(self)
+        return true
+    }
+    
 }
-
